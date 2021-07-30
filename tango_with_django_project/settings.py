7f7ab2dbd9d57b05362a126c37810ec73d577907
Tobilau04 <2546009l@student.gla.ac.uk>
2021-07-30 15:48:54 +0800

    chapter9_completed

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -130,4 +130,8 @@ STATICFILES_DIRS = [STATIC_DIR, ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
+# login url
+LOGIN_URL = 'rango:login'
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
